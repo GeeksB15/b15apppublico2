a447adbed85384ec5a71e33e22eeedbe977036e9
Carlos Neves Lenz Cesar <cnlenzc@gmail.com>
2024-10-18 11:26:53 -0300

    2024.1018.1116

--- assets/g.031ab827.js
+++
@@ -1,1 +0,0 @@
-import{b as a}from"./index.3260d026.js";const o={};var t=a(({app:g})=>{g.config.globalProperties.$g=o,globalThis.$g=o});export{t as default};
---
+++ assets/g.8742868a.js
@@ -0,0 +1,1 @@
+import{b as a}from"./index.b630ddbf.js";const o={};var t=a(({app:g})=>{g.config.globalProperties.$g=o,globalThis.$g=o});export{t as default};
