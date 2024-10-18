f70cd798aeb19a7fcf779ec937b2b93c27957b3b
Carlos Neves Lenz Cesar <cnlenzc@gmail.com>
2024-10-18 14:40:03 -0300

    2024.1018.1430

---
+++ assets/axios.7f59c6b7.js
@@ -0,0 +1,1 @@
+import{b as s,a as o}from"./index.cd63dbf9.js";import{a as e}from"./index.cd63dbf9.js";var r=s(({app:a})=>{a.config.globalProperties.$axios=o,globalThis.$axios=o});export{e as axios,r as default};
