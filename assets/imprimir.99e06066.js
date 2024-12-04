b87fa1382293f1c0db44b47671c46c000c9afacb
luiznogueira <luizetn@gmail.com>
2024-12-04 14:49:58 -0300

    2024.1204.1441

---
+++ assets/imprimir.99e06066.js
@@ -0,0 +1,1 @@
+import{b as n,aC as s,aD as b}from"./index.537352ed.js";var f=n(({app:r,router:e})=>{const o=function(l,i,m){i=i?typeof i=="object"?s.encodeToUrl(JSON.stringify(i)):s.encodeToUrl(i):"",b(m),e.push({name:"Imprimir",params:{nome:l,parametro:i}})};r.config.globalProperties.$imprimir=o,globalThis.$imprimir=o});export{f as default};
--- assets/utils.26072621.js
+++
@@ -1,1 +0,0 @@
-import{b as s,aJ as l}from"./index.aa2e3840.js";var a=s(({app:o})=>{o.config.globalProperties.$utils=l,globalThis.$utils=l});export{a as default};
