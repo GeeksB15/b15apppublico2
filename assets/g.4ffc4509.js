7cd1d2d82c7093f7d6ac39cfdd6901735eeabdc1
luiznogueira <luizetn@gmail.com>
2024-12-17 08:14:39 -0300

    2024.1217.0810

---
+++ assets/g.4ffc4509.js
@@ -0,0 +1,1 @@
+import{b as a}from"./index.b5bd9116.js";const o={};var t=a(({app:g})=>{g.config.globalProperties.$g=o,globalThis.$g=o});export{t as default};
--- assets/router.2cbd9846.js
+++
@@ -1,1 +0,0 @@
-import{b as t}from"./index.05370f25.js";var e=t(({app:r,router:o})=>{r.config.globalProperties.$router=o,globalThis.$router=o});export{e as default};
