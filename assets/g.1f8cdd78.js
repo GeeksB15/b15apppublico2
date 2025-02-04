7e96a7e1ced9b05b829ceca0be4e9031ffbccb80
Carlos Lenz <cnlenzc@gmail.com>
2025-02-04 10:36:10 -0300

    2025.0204.1029

---
+++ assets/g.1f8cdd78.js
@@ -0,0 +1,1 @@
+import{b as a}from"./index.71066ec2.js";const o={};var t=a(({app:g})=>{g.config.globalProperties.$g=o,globalThis.$g=o});export{t as default};
---
+++ assets/utils.16e8e543.js
@@ -0,0 +1,1 @@
+import{b as s,aJ as l}from"./index.71066ec2.js";var a=s(({app:o})=>{o.config.globalProperties.$utils=l,globalThis.$utils=l});export{a as default};
