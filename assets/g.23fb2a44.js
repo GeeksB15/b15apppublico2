25366833269db3879e8fdfb57c8285229415cb21
Carlos Lenz <cnlenzc@gmail.com>
2025-02-13 16:41:38 -0300

    2025.0213.1637

---
+++ assets/g.23fb2a44.js
@@ -0,0 +1,1 @@
+import{b as a}from"./index.edbe5130.js";const o={};var t=a(({app:g})=>{g.config.globalProperties.$g=o,globalThis.$g=o});export{t as default};
---
+++ assets/lodash.ddac41c1.js
@@ -0,0 +1,1 @@
+import{b as l,aE as o}from"./index.edbe5130.js";var e=l(({app:a})=>{a.config.globalProperties.$lodash=o,globalThis.$lodash=o,a.config.globalProperties.$get=o.get,globalThis.$get=o.get});export{e as default};
