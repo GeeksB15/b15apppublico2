545a8ad28bdafe358ae640200cbb463298049c7e
luiznogueira <luizetn@gmail.com>
2025-02-27 11:50:51 -0300

    2025.0227.1147

--- assets/lodash.2968130e.js
+++
@@ -1,1 +0,0 @@
-import{b as l,aE as o}from"./index.dfe785c2.js";var e=l(({app:a})=>{a.config.globalProperties.$lodash=o,globalThis.$lodash=o,a.config.globalProperties.$get=o.get,globalThis.$get=o.get});export{e as default};
--- assets/utils.8c06fadb.js
+++
@@ -1,1 +0,0 @@
-import{b as s,aJ as l}from"./index.dfe785c2.js";var a=s(({app:o})=>{o.config.globalProperties.$utils=l,globalThis.$utils=l});export{a as default};
