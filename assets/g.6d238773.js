ab4955faf5b74c8d84ca0b7cb668621a36e39315
luiznogueira <luizetn@gmail.com>
2025-04-29 15:26:50 -0300

    2025.0429.1522

--- assets/g.6d238773.js
+++
@@ -1,1 +0,0 @@
-import{b as a}from"./index.4f7ef08b.js";const o={};var t=a(({app:g})=>{g.config.globalProperties.$g=o,globalThis.$g=o});export{t as default};
---
+++ assets/g.ce8cf1f6.js
@@ -0,0 +1,1 @@
+import{b as a}from"./index.3e77a1c7.js";const o={};var t=a(({app:g})=>{g.config.globalProperties.$g=o,globalThis.$g=o});export{t as default};
