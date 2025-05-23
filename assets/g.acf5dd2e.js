32ed5e713bd8476dd1f19b06df4c7e918bea071c
Carlos Lenz <cnlenzc@gmail.com>
2025-05-23 11:28:35 -0300

    2025.0523.1124

---
+++ assets/g.acf5dd2e.js
@@ -0,0 +1,1 @@
+import{b as a}from"./index.df68757a.js";const o={};var t=a(({app:g})=>{g.config.globalProperties.$g=o,globalThis.$g=o});export{t as default};
--- assets/g.b444da7b.js
+++
@@ -1,1 +0,0 @@
-import{b as a}from"./index.7979b106.js";const o={};var t=a(({app:g})=>{g.config.globalProperties.$g=o,globalThis.$g=o});export{t as default};
