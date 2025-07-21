c877aa2cb503c8c5c528c200c9a10b301d8c615f
luiznogueira <luizetn@gmail.com>
2025-07-21 17:31:13 -0300

    2025.0721.1726

---
+++ assets/Branca.a373b5a6.js
@@ -0,0 +1,1 @@
+import{_ as e,o as r,p as a}from"./index.00f7be04.js";const o={};function t(c,n,s,p,_,f){return r(),a("div")}var i=e(o,[["render",t]]);export{i as default};
---
+++ assets/g.fdf17832.js
@@ -0,0 +1,1 @@
+import{b as a}from"./index.00f7be04.js";const o={};var t=a(({app:g})=>{g.config.globalProperties.$g=o,globalThis.$g=o});export{t as default};
