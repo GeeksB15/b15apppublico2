b84421d5726a84422053fabd10b18379b61069c7
luiznogueira <luizetn@gmail.com>
2025-08-14 09:52:55 -0300

    2025.0814.0948

---
+++ assets/g.db048696.js
@@ -0,0 +1,1 @@
+import{b as a}from"./index.69c58df2.js";const o={};var t=a(({app:g})=>{g.config.globalProperties.$g=o,globalThis.$g=o});export{t as default};
--- assets/router.8db8715b.js
+++
@@ -1,1 +0,0 @@
-import{b as t}from"./index.4b749224.js";var e=t(({app:r,router:o})=>{r.config.globalProperties.$router=o,globalThis.$router=o});export{e as default};
