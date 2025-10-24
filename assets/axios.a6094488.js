e0199a65606680746904d6f0d026208951439fcd
luiznogueira <luizetn@gmail.com>
2025-10-24 16:54:39 -0300

    2025.1024.1649

---
+++ assets/axios.a6094488.js
@@ -0,0 +1,1 @@
+import{b as s,a as o}from"./index.6cc9500b.js";import{a as e}from"./index.6cc9500b.js";var r=s(({app:a})=>{a.config.globalProperties.$axios=o,globalThis.$axios=o});export{e as axios,r as default};
--- assets/g.c9c466b6.js
+++
@@ -1,1 +0,0 @@
-import{b as a}from"./index.dc6d0659.js";const o={};var t=a(({app:g})=>{g.config.globalProperties.$g=o,globalThis.$g=o});export{t as default};
