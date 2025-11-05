a82ffe259567e2fcf87315164d3cdc06956dd393
luiznogueira <luizetn@gmail.com>
2025-11-05 11:15:54 -0300

    2025.1105.1112

--- assets/g.cf4d9749.js
+++
@@ -1,1 +0,0 @@
-import{b as a}from"./index.381ba721.js";const o={};var t=a(({app:g})=>{g.config.globalProperties.$g=o,globalThis.$g=o});export{t as default};
--- assets/router.2bf34a3f.js
+++
@@ -1,1 +0,0 @@
-import{b as t}from"./index.381ba721.js";var e=t(({app:r,router:o})=>{r.config.globalProperties.$router=o,globalThis.$router=o});export{e as default};
