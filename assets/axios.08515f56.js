ee55728c63b5ef6fea89d670fb6f2c53965d5f95
luiznogueira <luizetn@gmail.com>
2025-11-21 09:38:41 -0300

    2025.1121.0935

---
+++ assets/axios.08515f56.js
@@ -0,0 +1,1 @@
+import{b as s,a as o}from"./index.ad7f50a3.js";import{a as e}from"./index.ad7f50a3.js";var r=s(({app:a})=>{a.config.globalProperties.$axios=o,globalThis.$axios=o});export{e as axios,r as default};
---
+++ assets/utils.8a22261c.js
@@ -0,0 +1,1 @@
+import{b as s,aH as l}from"./index.ad7f50a3.js";var a=s(({app:o})=>{o.config.globalProperties.$utils=l,globalThis.$utils=l});export{a as default};
