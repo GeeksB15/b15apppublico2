6854ef512e746765f03824481da587d3ed5f487a
Carlos Lenz <cnlenzc@gmail.com>
2026-02-26 14:34:35 -0300

    2026.0226.1428

---
+++ assets/g.ba3aa8c2.js
@@ -0,0 +1,1 @@
+import{b as a}from"./index.6c538a71.js";const o={};var t=a(({app:g})=>{g.config.globalProperties.$g=o,globalThis.$g=o});export{t as default};
--- assets/lodash.53b5059f.js
+++
@@ -1,1 +0,0 @@
-import{b as l,aC as o}from"./index.653d4b7c.js";var e=l(({app:a})=>{a.config.globalProperties.$lodash=o,globalThis.$lodash=o,a.config.globalProperties.$get=o.get,globalThis.$get=o.get});export{e as default};
