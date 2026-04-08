385a84b2d6c6ee59528cb2756caceb4b1ce5709e
luiznogueira <luizetn@gmail.com>
2026-04-08 15:37:18 -0300

    2026.0408.1536

---
+++ assets/lodash.729a4fbf.js
@@ -0,0 +1,1 @@
+import{b as l,aC as o}from"./index.c2a8c61f.js";var e=l(({app:a})=>{a.config.globalProperties.$lodash=o,globalThis.$lodash=o,a.config.globalProperties.$get=o.get,globalThis.$get=o.get});export{e as default};
