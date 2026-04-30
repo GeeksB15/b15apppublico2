5826f3b6f7a353a617ed071f0fbf241c74398b76
luiznogueira <luizetn@gmail.com>
2026-04-30 10:57:42 -0300

    2026.0430.1055

--- assets/router.a1245100.js
+++
@@ -1,1 +0,0 @@
-import{b as t}from"./index.63b84ed3.js";var e=t(({app:r,router:o})=>{r.config.globalProperties.$router=o,globalThis.$router=o});export{e as default};
---
+++ assets/router.d48bd9d0.js
@@ -0,0 +1,1 @@
+import{b as t}from"./index.d6e39d5d.js";var e=t(({app:r,router:o})=>{r.config.globalProperties.$router=o,globalThis.$router=o});export{e as default};
