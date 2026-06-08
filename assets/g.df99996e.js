786a822c5d6d0041e8633f6a4183ac4d0f0f1dae
Carlos Lenz <cnlenzc@gmail.com>
2026-06-08 16:01:13 -0300

    2026.0608.1553

--- assets/g.df99996e.js
+++
@@ -1,1 +0,0 @@
-import{b as a}from"./index.ad7b3f01.js";const o={};var t=a(({app:g})=>{g.config.globalProperties.$g=o,globalThis.$g=o});export{t as default};
--- assets/router.ed621aa7.js
+++
@@ -1,1 +0,0 @@
-import{b as t}from"./index.ad7b3f01.js";var e=t(({app:r,router:o})=>{r.config.globalProperties.$router=o,globalThis.$router=o});export{e as default};
