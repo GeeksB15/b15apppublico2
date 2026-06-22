53c9109233b3942fc866d478cf5da188b5c9f286
Carlos Lenz <cnlenzc@gmail.com>
2026-06-22 10:43:22 -0300

    2026.0622.1038

--- assets/g.9deb477b.js
+++
@@ -1,1 +0,0 @@
-import{b as a}from"./index.54b52c20.js";const o={};var t=a(({app:g})=>{g.config.globalProperties.$g=o,globalThis.$g=o});export{t as default};
--- assets/utils.654b6b8d.js
+++
@@ -1,1 +0,0 @@
-import{b as s,aH as l}from"./index.54b52c20.js";var a=s(({app:o})=>{o.config.globalProperties.$utils=l,globalThis.$utils=l});export{a as default};
